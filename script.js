5329125f258929d5b8fee771cf8e61314b62952e
Dananjana Wijesooriya <dananjanawijesooriya@gmail.com>
2026-06-27 18:20:10 +0530

    Add navigation bar

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Portfolio website loaded successfully!")
+const navToggle=document.getElementById("navToggle");
+const navLinks=document.getElementById("navLinks");
+
+navToggle.addEventListener("click", ()=>{
+    navLinks.classList.toggle("active");
+});
