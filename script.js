491f51a0b59604ea320231b1fb73a06d046cd619
Dananjana Wijesooriya <dananjanawijesooriya@gmail.com>
2026-06-29 11:21:06 +0530

    Enhance UI with animations and smooth scrolling

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ const navLinks=document.getElementById("navLinks");
 navToggle.addEventListener("click", ()=>{
     navLinks.classList.toggle("active");
 });
+
+const section=document.querySelectorAll(".fade-in");
+
+const observer=new IntersectionObserver((entries) =>{
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+    })
+},{
+    threshold: 0.2
+});
+SpeechRecognitionResult.forEach((section)=>{
+    observer.observe(section);
+});
